e705d1a50144719f0bcb44001a359b805372d5ff
gerardonastri <gerardonastri06@gmail.com>
2023-04-16 12:08:05 +0200

    url

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,7 +1,9 @@
-import axios from "axios"
+import axios from "axios";
 
-const  BASE_URL = "https://jer-vegefoods-api.herokuapp.com/api/";
+// const  BASE_URL = "https://jer-vegefoods-api.herokuapp.com/api/";
+// const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = "https://vegefoods-api.onrender.com/api/";
 
 export const axiosReq = axios.create({
-    baseURL:  BASE_URL
-})
+  baseURL: BASE_URL,
+});
